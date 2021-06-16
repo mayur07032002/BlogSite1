a2695a8947a56ec16f7ec2a711eb8445b83b4f09
Hardik <hardikdharmik94@gmail.com>
2021-06-16 16:43:57 +0530

    This is second commit !!

--- BlogSite/views.py
+++ BlogSite/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("This is main page!!")
+    return HttpResponse("This is main page of our site!!")
