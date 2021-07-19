a7c329605447f143a00c22db67961228ab3cabe8
Mayur Rawte <66206145+mayur07032002@users.noreply.github.com>
2021-07-19 09:08:16 +0530

    Deployment

--- BlogSite/urls.py
+++ BlogSite/urls.py
@@ -4,6 +4,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 from . import views
 
+from django.views.static import serve
+from django.conf.urls import url
+
 urlpatterns = [
     path('change_user/',views.change_user,name="changeuser"),
     path('change_pass/',views.change_pass,name="changepass"),
@@ -21,4 +24,6 @@ urlpatterns = [
     path('chat/',include('chat.urls')),
     path('contact/',include('contact.urls')),
     path('videochat/',include('videochat.urls')),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
