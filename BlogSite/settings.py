f6290652a67c78ec1b507e2032c6132793a4df68
Mayur Rawte <66206145+mayur07032002@users.noreply.github.com>
2021-07-19 09:15:32 +0530

    added server

--- BlogSite/settings.py
+++ BlogSite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^#2x8ur&9m(5j*(4r_4x+!0(8zvsuaf)ldf-jiu)cvuwnhris#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','devcenter.heroku.com']
 
 
 # Application definition
